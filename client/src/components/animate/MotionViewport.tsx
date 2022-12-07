9f81b02ba4a0f00828671ca39223d8f0e81c7cc3
s184234 <s184234@student.dtu.dk>
2022-12-07 16:52:21 +0100

    Fixed sidebar disappearing on small screen size + other small improvements

--- client/src/components/animate/MotionViewport.tsx
+++ client/src/components/animate/MotionViewport.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from 'react';
 // @mui
 import { Box, BoxProps } from '@mui/material';
 // hooks
-import useResponsive from '../../hooks/useResponsive';
 //
 import { varContainer } from '.';
 
@@ -12,31 +11,26 @@ import { varContainer } from '.';
 type IProps = BoxProps & MotionProps;
 
 interface Props extends IProps {
-  children: ReactNode;
-  disableAnimatedMobile?: boolean;
+	children: ReactNode;
+	disableAnimatedMobile?: boolean;
 }
 
 export default function MotionViewport({
-  children,
-  disableAnimatedMobile = true,
-  ...other
-}: Props) {
-  const isMobile = useResponsive('down', 'sm');
+										   children,
+										   disableAnimatedMobile = true,
+										   ...other
+									   }: Props) {
 
-  if (isMobile && disableAnimatedMobile) {
-    return <Box {...other}>{children}</Box>;
-  }
-
-  return (
-    <Box
-      component={m.div}
-      initial="initial"
-      whileInView="animate"
-      viewport={{ once: true, amount: 0.3 }}
-      variants={varContainer()}
-      {...other}
-    >
-      {children}
-    </Box>
-  );
+	return (
+		<Box
+			component={m.div}
+			initial='initial'
+			whileInView='animate'
+			viewport={{ once: true, amount: 0.3 }}
+			variants={varContainer()}
+			{...other}
+		>
+			{children}
+		</Box>
+	);
 }
